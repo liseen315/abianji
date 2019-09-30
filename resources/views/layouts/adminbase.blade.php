899a558549444cd973742877af13eb624162db93
vagrant <vagrant@homestead>
2019-09-30 07:23:16 +0000

    add clockwork debuger

--- resources/views/layouts/adminbase.blade.php
+++ resources/views/layouts/adminbase.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Abianji | @yield('title')</title>
+    <link href="{{ mix('css/admin/admin.css') }}" rel="stylesheet">
+    @yield('css')
 </head>
-<body>
+<body class="hold-transition sidebar-mini layout-fixed">
 
 </body>
 </html>
