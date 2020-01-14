2e338329320de5246f3676515034087e06f63a6a
刘松 <ls@btctrade.com>
2020-01-14 10:24:11 +0800

    添加数据库备份

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\BackupDatabase::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('db:backup')->mondays()->at('23:00');
     }
 
     /**
